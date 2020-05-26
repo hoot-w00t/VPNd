a0b92ec61d12a635b264f1594a0614cfe58f2f53
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2020-05-26 21:10:25 +0200

    Remove useless prototypes in peer.h

--- include/peer.h
+++ include/peer.h
@@ -6,8 +6,6 @@
 #define _VPND_PEER
 
 void destroy_peers(void);
-void *peer_receive(void *arg);
-void *_peer_connection(void *arg);
 void peer_connection(struct sockaddr_in *sin, int s, bool is_client, bool block);
 void broadcast_data_to_peers(uint8_t *data, size_t n, peer_t *exclude);
 void broadcast_tuntap_device(bool block);
