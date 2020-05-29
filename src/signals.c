509325775938043524925cbbcb68d50d6e73a130
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2020-05-29 12:18:44 +0200

    Catch USR1 signal to dump connected peers

--- src/signals.c
+++ src/signals.c
@@ -1,6 +1,7 @@
 #include "peer_net.h"
 #include "tcp.h"
 #include "attributes.h"
+#include "peer.h"
 #include <stdio.h>
 #include <signal.h>
 
@@ -11,7 +12,13 @@ void interrupt_program_handler(UNUSED int sig)
     broadcast_close();
 }
 
+void usr1_handler(UNUSED int sig)
+{
+    dump_peers();
+}
+
 void override_signals(void)
 {
     signal(SIGINT, &interrupt_program_handler);
+    signal(SIGUSR1, &usr1_handler);
 }
